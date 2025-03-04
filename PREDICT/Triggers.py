cbe6feef26a3a39205743141e632583be5a1e374
ZoeHancox <z.hancox@outlook.com>
2025-03-04 14:11:43 +0000

    DocString added

--- PREDICT/Triggers.py
+++ PREDICT/Triggers.py
@@ -6,10 +6,15 @@ def AccuracyThreshold(model, threshold, prediction_threshold=0.5):
     return MethodType(lambda self, x: __AccuracyThreshold(self, x, threshold, prediction_threshold), model)
 
 def __AccuracyThreshold(self, input_data, threshold, prediction_threshold):
-    """
-    Trigger function to update model if accuracy falls below a given threshold.
-    
-    TODO: Fill in
+    """Trigger function to update model if accuracy falls below a given threshold.
+
+    Args:
+        input_data (dataframe): DataFrame with column of the predicted outcome.
+        threshold (float, optional): Probability threshold at which to classify individuals. Defaults to 0.5.
+        prediction_threshold (float): Static accuracy threshold to trigger model update. 
+
+    Returns:
+        bool: Returns True if model update is required.
     """
     preds = self.predict(input_data)
     outcomes = input_data[self.outcomeColName].astype(int)
