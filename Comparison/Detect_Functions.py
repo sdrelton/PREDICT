4bbacc0a295b31f92e442e5e026f6c55ee55e493
ZoeHancox <z.l.hancox@leeds.ac.uk>
2025-05-29 10:24:25 +0100

    Docstrings added to functions

--- Comparison/Detect_Functions.py
+++ Comparison/Detect_Functions.py
@@ -11,6 +11,19 @@ from PREDICT.Plots import *
 
 
 def get_model_updated_log_covid(df, model, switch_times, i, model_name, undetected):
+    """Get the model update log for a given model and switch time for the COVID simulation data.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the simulation data.
+        model (PREDICTModel or bmb.Model): PREDICT or bayesian model to be used for prediction.
+        switch_times (str): List of switch times for the model updates.
+        i (int): Index for the switch time to check.
+        model_name (str): Name of the model being checked for updates.
+        undetected (dict): Dictionary to keep track of undetected models and their counts.
+
+    Returns:
+        int: time to detect (ttd) in days, or None if no model update detected.
+    """
     mytest = PREDICT(data=df, model=model, startDate='min', endDate='max', timestep='month')
     mytest.run()
     log = mytest.getLog()
@@ -41,6 +54,19 @@ def get_model_updated_log_covid(df, model, switch_times, i, model_name, undetect
         return ttd
     
 def get_model_updated_log_prev_drift(df, model, startDate, model_name, undetected):
+    """Get the model update log for a given model and switch time for the sslow change, 
+    outcome prevalence drift, and QRISK simulation data.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the simulation data.
+        model (PREDICTModel or bmb.Model): PREDICT or bayesian model to be used for prediction.
+        startDate (datetime64[ns]): Start date for when the data period begins.
+        model_name (str): Name of the model being checked for updates.
+        undetected (dict): Dictionary to keep track of undetected models and their counts.
+
+    Returns:
+        int: time to detect (ttd) in days, or None if no model update detected.
+    """
     mytest = PREDICT(data=df, model=model, startDate='min', endDate='max', timestep='month')
     mytest.run()
     log = mytest.getLog()
@@ -93,6 +119,14 @@ def get_binom_from_normal(mean, std, num_patients, threshold):
     return binom_dist
 
 def select_ethnic_group(num_patients):
+    """ Randomly assign each patient to an ethnic group based on predefined probabilities.
+
+    Args:
+        num_patients (int): Number of patients to select an ethnic group for.
+
+    Returns:
+        dict: Dictionary of ethnic groups with binary assignments for each patient.
+    """
     # Define ethnicity groups
     ethnic_groups = ["White", "Indian", "Pakistani", "Bangladeshi", "Other_Asian",
                     "Black_Caribbean", "Black_African", "Chinese", "Other"]
@@ -115,22 +149,33 @@ def select_ethnic_group(num_patients):
 
     return ethnicity_assignments
 
-def plot_prev_over_time(df, switchDateStrings, regular_ttd, static_ttd, spc_ttd3, spc_ttd5, spc_ttd7, bayesian_ttd):    
+def plot_prev_over_time(df, switchDateStrings, regular_ttd, static_ttd, spc_ttd3, spc_ttd5, spc_ttd7, bayesian_ttd):
+    """Plot the prevalence of an outcome over time, with vertical lines indicating model update times.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the simulation data with 'date' and 'outcome' columns.
+        switchDateStrings (list or None): List of switch dates as strings, or None if not applicable.
+        regular_ttd (list): List of time to detect (ttd) for regular testing model updates.
+        static_ttd (list): List of time to detect (ttd) for static threshold model updates.
+        spc_ttd3 (list): List of time to detect (ttd) for SPC 3 months model updates.
+        spc_ttd5 (list): List of time to detect (ttd) for SPC 5 months model updates.
+        spc_ttd7 (list): List of time to detect (ttd) for SPC 7 months model updates.
+        bayesian_ttd (list): List of time to detect (ttd) for Bayesian model updates.
+    """
+
     # If we want to plot a different simulated data prevalence:
     # save times and grouped prevalence in a df to plot at the end? - another column to say which number run it is for new lines
-
-    # plot prevalence over time for each switchTime - start with just the final one first
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 5)) # plot prevalence over time for each switchTime - start with just the final one first
 
     # groupby the date and get the sum of the outcome
     groupby_df = df.groupby('date').agg({'outcome': 'sum'}).reset_index()
 
     plt.plot(groupby_df['date'], groupby_df['outcome'], label='Prevalence', color='blue')
 
-
-    # get the model update times and plot them as vertical lines
-    switch_time = pd.to_datetime(switchDateStrings[-1], dayfirst=True)
-    plt.vlines(x=switch_time, ymin=0, ymax=groupby_df['outcome'].max(), color='orange', linestyle='--', label='Switch Time')
+    if switchDateStrings is not None:
+        # get the model update times and plot them as vertical lines
+        switch_time = pd.to_datetime(switchDateStrings[-1], dayfirst=True)
+        plt.vlines(x=switch_time, ymin=0, ymax=groupby_df['outcome'].max(), color='orange', linestyle='--', label='Switch Time')
 
     if len(regular_ttd) > 0 and regular_ttd[-1] is not None:
         regular_update = switch_time + timedelta(days=regular_ttd[-1])
@@ -158,6 +203,29 @@ def plot_prev_over_time(df, switchDateStrings, regular_ttd, static_ttd, spc_ttd3
 
 
 def run_recalibration_tests(df, startDate, undetected, total_runs, regular_ttd, static_ttd, spc_ttd3, spc_ttd5, spc_ttd7, recalthreshold):
+    """Run recalibration tests on the given DataFrame using various triggers and return the updated undetected counts and time to detect (ttd) for each method.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the simulation data with 'date' and 'outcome' columns.
+        startDate (datetime[ns]): Start date for when the data period begins.
+        undetected (dict): Dictionary to keep track of undetected models and their counts.
+        total_runs (int):  Total number of runs performed so far.
+        regular_ttd (list): List of time to detect (ttd) for regular testing model updates.
+        static_ttd (list): List of time to detect (ttd) for static threshold model updates.
+        spc_ttd3 (list): List of time to detect (ttd) for SPC 3 months model updates.
+        spc_ttd5 (list): List of time to detect (ttd) for SPC 5 months model updates.
+        spc_ttd7 (list): List of time to detect (ttd) for SPC 7 months model updates.
+        recalthreshold (float): Threshold for the static threshold recalibration method.
+
+    Returns:
+        dict: Dictionary of undetected models and their counts.
+        int: Total number of runs performed.
+        list: List of time to detect (ttd) for regular testing model updates.
+        list: List of time to detect (ttd) for static threshold model updates.
+        list: List of time to detect (ttd) for SPC 3 months model updates.
+        list: List of time to detect (ttd) for SPC 5 months model updates.
+        list: List of time to detect (ttd) for SPC 7 months model updates.
+    """
     ########################## Regular Testing ##########################
     model = RecalibratePredictions()
     model.trigger = TimeframeTrigger(model=model, updateTimestep=100, dataStart=df['date'].min(), dataEnd=df['date'].max())
@@ -186,8 +254,28 @@ def run_recalibration_tests(df, startDate, undetected, total_runs, regular_ttd, 
     spc_ttd7.append(ttd)
     return undetected, total_runs, regular_ttd, static_ttd, spc_ttd3, spc_ttd5, spc_ttd7
 
+
+
 def run_bayes_model(undetected, bay_model, bayes_dict, df, bayesian_ttd, switchDateStrings, switchDateidx, sim_data="covid"):
-    bay_model.trigger = BayesianRefitTrigger(model=bay_model, input_data=df, refitFrequency=18)
+    """Run the Bayesian model with a refit trigger and return the updated undetected counts, time to detect (ttd), and coefficients.
+
+    Args:
+        undetected (dict): Dictionary to keep track of undetected models and their counts.
+        bay_model (bmb.Model): Bayesian model to be used for prediction.
+        bayes_dict (dict): Dictionary to store Bayesian coefficients and other information.
+        df (pd.DataFrame): DataFrame containing the simulation data with 'date' and 'outcome' columns.
+        bayesian_ttd (list): List to store time to detect (ttd) for Bayesian model updates.
+        switchDateStrings (list or datetime[ns]): List of switch dates as strings for the model updates or datetime[ns] if startDate is
+                                given for non-COVID data simulation.
+        switchDateidx (int): Index of the switch date to check for model updates.
+        sim_data (str, optional): String of the data simulation type. Defaults to "covid".
+
+    Returns:
+        dict: Dictionary of undetected models and their counts.
+        list: List of time to detect (ttd) for Bayesian model updates.
+        dict: Dictionary of Bayesian coefficients and other information.
+    """
+    bay_model.trigger = BayesianRefitTrigger(model=bay_model, input_data=df, refitFrequency=1)
     mytest = PREDICT(data=df, model=bay_model, startDate='min', endDate='max', timestep='month')
     mytest.addLogHook(TrackBayesianCoefs(bay_model))
     mytest.run()
