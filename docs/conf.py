577d7973de0752507bf1f18d52776bd90898b21a
ZoeHancox <z.l.hancox@leeds.ac.uk>
2025-12-17 10:15:21 +0000

    Change theme of website to have dark and light modes

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "fuo"#'alabaster'
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#007acc",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#66ccff",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
